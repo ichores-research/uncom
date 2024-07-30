1e85feecb1e760e569100216708c484889a820be
Pawel Gajewski <lubiluk@gmail.com>
2024-07-30 15:48:59 +0200

    mediapipe test

---
+++ mediapipe_test.py
@@ -0,0 +1,29 @@
+# STEP 1: Import the necessary modules.
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+from utils import draw_landmarks_on_image
+
+# STEP 2: Create an HandLandmarker object.
+base_options = python.BaseOptions(model_asset_path="models/hand_landmarker.task")
+options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+detector = vision.HandLandmarker.create_from_options(options)
+
+# STEP 3: Load the input image.
+image = mp.Image.create_from_file("data/apple.jpg")
+
+# STEP 4: Detect hand landmarks from the input image.
+detection_result = detector.detect(image)
+
+# STEP 5: Process the classification result. In this case, visualize it.
+annotated_image = draw_landmarks_on_image(image.numpy_view()[:,:,:3], detection_result)
+
+# Interesting ones are:
+# Palm: 0 -> 9
+# Finger: 5 -> 8
+
+cv2.imshow("test", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
